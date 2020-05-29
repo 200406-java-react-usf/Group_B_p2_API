c1ecb53bc93cabe65831c463b3115fc93ac20ff6
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-28 20:59:33 -0700

    added basic folders, added repository for user invoice and inventory, also added services for inventory, invoice, and user

---
+++ src/main/java/com/revature/memestore/services/InventoryService.java
@@ -0,0 +1,32 @@
+package com.revature.memestore.services;
+
+import com.revature.memestore.repos.CrudRepository;
+
+import java.util.List;
+
+public class InventoryService implements CrudRepository {
+    @Override
+    public List getAll() {
+        return null;
+    }
+
+    @Override
+    public Object findById(int id) {
+        return null;
+    }
+
+    @Override
+    public Object save(Object newObj) {
+        return null;
+    }
+
+    @Override
+    public boolean update(Object updatedObj) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return false;
+    }
+}
