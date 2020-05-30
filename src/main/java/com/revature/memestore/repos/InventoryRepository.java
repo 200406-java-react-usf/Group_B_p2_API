ccd47fba5071e1fa14cb937fdb64727aebf261ad
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-29 23:19:02 -0700

    added basic set up for Inventory Repository and implemented a getall() method inside the repo

--- src/main/java/com/revature/memestore/repos/InventoryRepository.java
+++ src/main/java/com/revature/memestore/repos/InventoryRepository.java
@@ -1,4 +1,42 @@
 package com.revature.memestore.repos;
 
-public class InventoryRepository {
+
+import com.revature.memestore.models.Inventory;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class InventoryRepository implements CrudRepository<Inventory> {
+
+    private SessionFactory sessionFactory;
+
+
+    @Override
+    public List<Inventory> getAll() {
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("from inventory", Inventory.class).getResultList();
+    }
+
+    @Override
+    public Inventory findById(int id) {
+        return null;
+    }
+
+    @Override
+    public Inventory save(Inventory newObj) {
+        return null;
+    }
+
+    @Override
+    public boolean update(Inventory updatedObj) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return false;
+    }
 }
