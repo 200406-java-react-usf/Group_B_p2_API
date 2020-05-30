14ddec7d5b68503fc69cf79974e31fa69a53c04b
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-30 13:51:10 -0700

    added functionality and checks for getAll, findById, save, and delteById methods in InventoryService

--- src/main/java/com/revature/memestore/services/InventoryService.java
+++ src/main/java/com/revature/memestore/services/InventoryService.java
@@ -1,32 +1,75 @@
 package com.revature.memestore.services;
 
+import com.revature.memestore.exceptions.BadRequestException;
+import com.revature.memestore.exceptions.ResourceNotFoundException;
+import com.revature.memestore.models.Inventory;
 import com.revature.memestore.repos.CrudRepository;
+import com.revature.memestore.repos.InventoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-public class InventoryService implements CrudRepository {
-    @Override
-    public List getAll() {
-        return null;
+import static java.lang.Float.NaN;
+
+@Service
+public class InventoryService implements CrudRepository<Inventory> {
+
+    private InventoryRepository inventoryRepository;
+
+
+    @Autowired
+    public InventoryService(InventoryRepository repo){
+        super();
+        this.inventoryRepository = repo;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Inventory> getAll() {
+        List<Inventory> inventory = inventoryRepository.getAll();
+        if(inventory.size() == 0 ){
+            throw new ResourceNotFoundException("There are no items in the Inventory");
+        }
+        return inventory;
+    }
+
+    @Transactional(readOnly = true)
+    public Inventory findById(int id) {
+        if(id < 1 || Float.isNaN(id)){
+            throw new BadRequestException("Invalid Id was provided");
+        }
+        Inventory item = inventoryRepository.findById(id);
+
+        if(item == null){
+            throw new ResourceNotFoundException("No item found with provided Id");
+        }
+        return item;
+    }
+
+    @Transactional
+    public Inventory save(Inventory newObj) {
+        if(newObj == null || newObj.getDetails().equals("") || newObj.getCategory().equals("")
+            || newObj.getItem_image().equals("") || newObj.getItem_name().equals("")){
+            throw new BadRequestException("Invalid Object Provided");
+        }
+
+
+        return inventoryRepository.save(newObj);
     }
 
     @Override
-    public Object findById(int id) {
-        return null;
-    }
+    public boolean update(Inventory updatedObj) {
 
-    @Override
-    public Object save(Object newObj) {
-        return null;
-    }
-
-    @Override
-    public boolean update(Object updatedObj) {
         return false;
     }
 
-    @Override
+    @Transactional
     public boolean deleteById(int id) {
-        return false;
+        if(id < 1 || Float.isNaN(id)){
+            throw new BadRequestException("Invalid Id was provided");
+        }
+        inventoryRepository.deleteById(id);
+        return true;
     }
 }
