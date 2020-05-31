5c567e229929dad80dc27c76b3baf3f1d636088a
Wagenheim <wagenheimk@me.com>
2020-05-30 20:29:47 -0400

    implemented get all

--- src/main/java/com/revature/memestore/services/UserService.java
+++ src/main/java/com/revature/memestore/services/UserService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -15,6 +17,13 @@ public class UserService {
     public UserService(UserRepository userRepository){
 
         this.userRepo = userRepository;
+
+    }
+
+    @Transactional
+    public List<User> getAllUsers(){
+
+        return userRepo.getAll();
 
     }
 
--- src/main/java/com/revature/memestore/web/controllers/UserController.java
+++ src/main/java/com/revature/memestore/web/controllers/UserController.java
@@ -4,10 +4,10 @@ import com.revature.memestore.models.User;
 import com.revature.memestore.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -19,6 +19,13 @@ public class UserController {
     public UserController(UserService userService){
 
         this.userService = userService;
+
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<User> getAllUsers(HttpServletRequest req){
+
+        return userService.getAllUsers();
 
     }
 
