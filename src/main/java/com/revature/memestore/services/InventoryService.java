647ac310b7e84b8d51e2c2566cbecece8c0c5af5
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-30 21:46:13 -0700

    commented out all the validation logic in the services to make sure endpoint can be hit properly

--- src/main/java/com/revature/memestore/services/InventoryService.java
+++ src/main/java/com/revature/memestore/services/InventoryService.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static java.lang.Float.NaN;
 
 @Service
-public class InventoryService implements CrudRepository<Inventory> {
+public class InventoryService {
 
     private InventoryRepository inventoryRepository;
 
@@ -26,50 +26,51 @@ public class InventoryService implements CrudRepository<Inventory> {
     }
 
     @Transactional(readOnly = true)
-    public List<Inventory> getAll() {
+    public List<Inventory> getAllItems() {
         List<Inventory> inventory = inventoryRepository.getAll();
-        if(inventory.size() == 0 ){
-            throw new ResourceNotFoundException("There are no items in the Inventory");
-        }
+//        if(inventory.size() == 0 ){
+//            throw new ResourceNotFoundException("There are no items in the Inventory");
+//        }
         return inventory;
     }
 
     @Transactional(readOnly = true)
-    public Inventory findById(int id) {
-        if(id < 1 || Float.isNaN(id)){
-            throw new BadRequestException("Invalid Id was provided");
-        }
+    public Inventory getInvoiceById(int id) {
+//        if(id < 1 || Float.isNaN(id)){
+//            throw new BadRequestException("Invalid Id was provided");
+//        }
         Inventory item = inventoryRepository.findById(id);
 
-        if(item == null){
-            throw new ResourceNotFoundException("No item found with provided Id");
-        }
+//        if(item == null){
+//            throw new ResourceNotFoundException("No item found with provided Id");
+//        }
         return item;
     }
 
     @Transactional
-    public Inventory save(Inventory newObj) {
-        if(newObj == null || newObj.getDetails().equals("") || newObj.getCategory().equals("")
-            || newObj.getItem_image().equals("") || newObj.getItem_name().equals("")){
-            throw new BadRequestException("Invalid Object Provided");
-        }
-
+    public Inventory saveNewInventory(Inventory newObj) {
+//        if(newObj == null || newObj.getDetails().equals("") || newObj.getCategory().equals("")
+//            || newObj.getItem_image().equals("") || newObj.getItem_name().equals("")){
+//            throw new BadRequestException("Invalid Object Provided");
+//        }
 
         return inventoryRepository.save(newObj);
     }
 
-    @Override
-    public boolean update(Inventory updatedObj) {
+    @Transactional
+    public boolean updateInventory(Inventory updatedInventory) {
 
-        return false;
+        //Validation
+
+        return inventoryRepository.update(updatedInventory);
     }
 
     @Transactional
-    public boolean deleteById(int id) {
-        if(id < 1 || Float.isNaN(id)){
-            throw new BadRequestException("Invalid Id was provided");
-        }
-        inventoryRepository.deleteById(id);
-        return true;
+    public boolean deleteInventoryById(int id) {
+//        if(id < 1 || Float.isNaN(id)){
+//            throw new BadRequestException("Invalid Id was provided");
+//        }
+        ;
+        return inventoryRepository.deleteById(id);
     }
 }
