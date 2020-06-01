a33b42204689d9463e0a21079b34621767ed8330
Wagenheim <wagenheimk@me.com>
2020-06-01 17:24:26 -0400

    set up enum

--- src/main/java/com/revature/memestore/models/UserRole.java
+++ src/main/java/com/revature/memestore/models/UserRole.java
@@ -1,4 +1,47 @@
 package com.revature.memestore.models;
 
 public enum UserRole {
+
+    MANAGER("Manager"), USER("User"), LOCKED("Locked");
+
+    private final String roleName;
+
+    UserRole(String name) {
+        this.roleName = name;
+    }
+
+    public static UserRole getByName(String name){
+
+        for (UserRole role : UserRole.values()){
+
+            if(role.roleName.equals(name)){
+                return role;
+            }
+
+        }
+
+        return LOCKED;
+
+    }
+
+    public static UserRole getById(int id){
+
+        for (UserRole role : UserRole.values()){
+
+            if(role.ordinal() == id){
+                return role;
+            }
+
+        }
+
+        return LOCKED;
+
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole{" +
+                "name='" + roleName + '\'' +
+                '}';
+    }
 }
