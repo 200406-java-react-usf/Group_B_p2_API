34c5f25a5943173514994cb58104c028176ab63c
jeremy2975 <=Jeremy2975@yahoo.com>
2020-06-02 18:32:43 -0700

    removed ghetto cors filter

--- src/main/java/com/revature/memestore/AppConfig.java
+++ src/main/java/com/revature/memestore/AppConfig.java
@@ -13,7 +13,6 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -97,10 +96,6 @@ public class AppConfig implements WebMvcConfigurer, WebApplicationInitializer {
         dispatcher.addMapping("/");
 
     }
-    @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**").allowedMethods("GET","POST","PUT","DELETE");
-        registry.addMapping("/**").allowCredentials(true);
-    }
+   
 
 }
