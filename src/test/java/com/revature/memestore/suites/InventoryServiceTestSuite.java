d1527533a586201cfeb29719f8d7426f0e4721e4
jeremy2975 <=Jeremy2975@yahoo.com>
2020-06-07 09:31:47 -0700

    added test suite to testing

---
+++ src/test/java/com/revature/memestore/suites/InventoryServiceTestSuite.java
@@ -0,0 +1,13 @@
+package com.revature.memestore.suites;
+
+
+import com.revature.memestore.services.InventoryServiceTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        InventoryServiceTest.class
+})
+public class InventoryServiceTestSuite {
+}
