4174d22853700288149e635b45d5f6db1290c29d
Wagenheim <wagenheimk@me.com>
2020-06-07 18:28:37 -0400

    added othe

--- src/test/java/com/revature/memestore/suites/UserServiceTestSuite.java
+++ src/test/java/com/revature/memestore/suites/UserServiceTestSuite.java
@@ -1,12 +1,16 @@
 package com.revature.memestore.suites;
 
+import com.revature.memestore.services.InventoryServiceTest;
+import com.revature.memestore.services.InvoiceServiceTest;
 import com.revature.memestore.services.UserServiceTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        UserServiceTest.class
+        UserServiceTest.class,
+        InvoiceServiceTest.class,
+        InventoryServiceTest.class
 })
 public class UserServiceTestSuite {
 }
