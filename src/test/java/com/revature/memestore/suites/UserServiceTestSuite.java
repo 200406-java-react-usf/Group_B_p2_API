e5904267b5ed99cf97787b9fb95aabed7dc67a37
Wagenheim <wagenheimk@me.com>
2020-06-06 20:45:20 -0400

    began testing userService

---
+++ src/test/java/com/revature/memestore/suites/UserServiceTestSuite.java
@@ -0,0 +1,12 @@
+package com.revature.memestore.suites;
+
+import com.revature.memestore.services.UserServiceTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        UserServiceTest.class
+})
+public class UserServiceTestSuite {
+}
